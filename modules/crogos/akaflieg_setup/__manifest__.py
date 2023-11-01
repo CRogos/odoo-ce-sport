47adfc9979517c50ce4569a3f19477d0b6ad3bdf
CRogos <crogos@gmail.com>
2023-11-01 21:59:50 +0100

    [ADD] akaflieg_setup

---
+++ modules/crogos/akaflieg_setup/__manifest__.py
@@ -0,0 +1,14 @@
+# Copyright 2016 Acsone SA/NV
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Akaflieg Frankfurt Setup",
+    "summary": "Install all dependend modules",
+    "version": "16.0.1.0.0",
+    "license": "AGPL-3",
+    "category": "Accounting",
+    "author": "CRogos",
+    "website": "https://github.com/CRogos/odoo-ce-sport",
+    "depends": ["account"],
+    "data": [],
+}
